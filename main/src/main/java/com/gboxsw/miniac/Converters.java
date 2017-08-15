2f4d3fbe6d006fecc72c2d9001c3845e2e3b0f99
František Galčík <frantisek.galcik@gbox.sk>
2017-08-15 17:19:29 +0200

    Add common converter singletons.

---
+++ main/src/main/java/com/gboxsw/miniac/Converters.java
@@ -0,0 +1,32 @@
+package com.gboxsw.miniac;
+
+import com.gboxsw.miniac.converters.BooleanToTextConverter;
+import com.gboxsw.miniac.converters.DoubleToTextConverter;
+import com.gboxsw.miniac.converters.IntToTextConverter;
+import com.gboxsw.miniac.converters.LongToTextConverter;
+
+/**
+ * Collection of commonly used converters.
+ */
+public class Converters {
+
+	/**
+	 * Default boolean to text converter.
+	 */
+	public static final BooleanToTextConverter BOOL2TEXT = new BooleanToTextConverter("1", "0", false);
+
+	/**
+	 * Default long to text converter.
+	 */
+	public static final LongToTextConverter LONG2TEXT = new LongToTextConverter();
+
+	/**
+	 * Default int to text converter.
+	 */
+	public static final IntToTextConverter INT2TEXT = new IntToTextConverter();
+
+	/**
+	 * Default double to text converter.
+	 */
+	public static final DoubleToTextConverter DOUBLE2TEXT = new DoubleToTextConverter();
+}
